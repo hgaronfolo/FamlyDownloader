c030e7c2072dee8fbb8829aacc91dbf74958f496
aarislarsen <aarislarsen@gmail.com>
2021-02-01 13:26:45 +0100

    Update FamlyDownloader.ps1

--- FamlyDownloader.ps1
+++ FamlyDownloader.ps1
@@ -11,6 +11,10 @@ Write-Host -ForegroundColor Green "  af Novas og Vidas far|___/ "
 
 $username = Read-Host -Prompt "Indtast Famly brugernavn/email og tryk Enter"
 $password = Read-Host -Prompt "Indtast Famly password og tryk Enter"
+$choice = Read-Host -Prompt "Tryk u for at downloade billeder af udmeldte børn, i for indmeldte børn, og tryk Enter"
+
+
+
 
 Write-Host "Logger ind... "
 $Server = 'app.famly.co'
@@ -21,38 +25,75 @@ $Body = @{
     deviceid = "null"
     locale = "en-US"
 }
+
 $accessToken = Invoke-RestMethod -Method Post -uri $Url -Body $Body | select -ExpandProperty accessToken
-
-
-
-Write-Host "Finder børn... "
-$Url = "https://${server}/api/me/me/me?accessToken=${accessToken}"
-
-$kids = Invoke-RestMethod -Method Get -Uri $Url | select -ExpandProperty roles2 |select -ExpandProperty targetId
-
 
 $headers = @{
         'x-famly-accesstoken' = $accessToken
     }
 
 
-ForEach ($line in $($kids -split "`r`n"))
+
+
+
+$Url = "https://${server}/api/me/me/me?accessToken=${accessToken}"
+
+
+
+if($choice -eq 'u')
 {
-    Write-Host "Finder billed oversigt for ${line}... " 
-    $url = "https://${server}/api/v2/images/tagged?childId=${Line}"
-    $images = Invoke-RestMethod -Method Get -Uri $Url -Headers $headers
+    
+    Write-Host "Finder udmeldte børn..."
+    $kids = Invoke-RestMethod -Method Get -Uri $Url | Select behaviors | ConvertTo-Json -Depth 4 | ConvertFrom-Json
 
-    Write-Host "Opretter mappe ${line}..."
-    $out = Get-Location
-    New-Item -Path $out -Name ${line} -ItemType "directory"
 
-    Write-Host "Downloader billeder"
-    for( $i = 0; $i -lt $images.Count ; $i++)
-    {        
-        $location = $images.GetValue($i) | select -ExpandProperty url_big
-        Write-Host -NoNewline "Downloader "
-        Write-Host -ForegroundColor Green $location
-        wget -Uri $location -OutFile ${out}\${line}\${i}.jpg
+
+    foreach ($kid in ($kids[0].behaviors[3].payload | select -ExpandProperty children))
+    {
+        $currentKid = $kid.Substring(10,36)
+
+        Write-Host "Finder billed oversigt for ${currentKid}... " 
+        $url = "https://${server}/api/v2/images/tagged?childId=${currentKid}"
+        $images = Invoke-RestMethod -Method Get -Uri $Url -Headers $headers
+
+        Write-Host "Opretter mappe ${currentKid}..."
+        $out = Get-Location
+        New-Item -Path $out -Name ${currentKid} -ItemType "directory"
+
+        Write-Host "Downloader billeder"
+        for( $i = 0; $i -lt $images.Count ; $i++)
+        {        
+            $location = $images.GetValue($i) | select -ExpandProperty url_big
+            Write-Host -NoNewline "Downloader "
+            Write-Host -ForegroundColor Green $location
+            wget -Uri $location -OutFile ${out}\${currentKid}\${i}.jpg
+        }
+        Write-Host "Download udført"
     }
-    Write-Host "Download udført"
+}
+else
+{
+    Write-Host "Finder børn... "
+    $kids = Invoke-RestMethod -Method Get -Uri $Url | select -ExpandProperty roles2 |select -ExpandProperty targetId
+
+    ForEach ($line in $($kids -split "`r`n"))
+    {
+        Write-Host "Finder billed oversigt for ${line}... " 
+        $url = "https://${server}/api/v2/images/tagged?childId=${Line}"
+        $images = Invoke-RestMethod -Method Get -Uri $Url -Headers $headers
+
+        Write-Host "Opretter mappe ${line}..."
+        $out = Get-Location
+        New-Item -Path $out -Name ${line} -ItemType "directory"
+
+        Write-Host "Downloader billeder"
+        for( $i = 0; $i -lt $images.Count ; $i++)
+        {        
+            $location = $images.GetValue($i) | select -ExpandProperty url_big
+            Write-Host -NoNewline "Downloader "
+            Write-Host -ForegroundColor Green $location
+            wget -Uri $location -OutFile ${out}\${line}\${i}.jpg
+        }
+        Write-Host "Download udført"
+    }
 }
